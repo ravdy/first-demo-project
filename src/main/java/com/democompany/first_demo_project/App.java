e991d7e8c8303f770597afc3baf9915ea736a6f1
unknown <Ravdy>
2021-03-12 12:20:49 +0530

    added initial commit

---
+++ src/main/java/com/democompany/first_demo_project/App.java
@@ -0,0 +1,13 @@
+package com.democompany.first_demo_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
